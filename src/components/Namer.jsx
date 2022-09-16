1a34dc35ad5d92d50057e815a7583bd58bad83dc
blockstar-jarod\Jarod Evans <jarod@blockstars.com.au>
2022-09-16 10:58:06 +1000

    Adding the useEffect

--- src/components/Namer.jsx
+++ src/components/Namer.jsx
@@ -4,21 +4,21 @@ import { useState } from 'react';
 
 const Namer = () => {
     const [name, setName] = useState("default name");
-    const [count, setCount] = useState(1);
     const names = ["Excalibur", "Renauld", "Gerard", "Lancelot", "Arthur"];
 
     function changeName(counter){
-        if(counter>=names.length-1){
-            setCount(0);
-        }else{
-            setCount(counter+1);
-        }
         return(
             setName(names[counter])
         );
     }
 
-    
+    const [useCount, setUseCount] = useState(0);
+    useEffect(() => {
+        if(useCount>names.length-1){
+            setUseCount(0);
+        }
+        changeName(useCount)
+    }, [useCount])
 
     return ( 
         <div className='mt-5 justify-center text-center content-center'>
@@ -26,7 +26,7 @@ const Namer = () => {
                 <h1 className='py-3 px-3 text-slate-800 font-mono font-bold text-3xl'>The namer: "{name}"</h1>
                 <div>
                 <p className='py-3 px-3 mt-5 mb-4 justify-center text-center content-center font-semibold font-sans text-4xl'>Click the button to change the name using useEffect.</p>
-                <button className='bg-red-500 rounded-lg py-2 px-3 text-white font-semibold text-6xl' onClick={() => changeName(count)}>Click</button>
+                <button className='bg-red-500 rounded-lg py-2 px-3 text-white font-semibold text-6xl' onClick={() => setUseCount(useCount+1)}>Click</button>
                 </div>
             </div> 
         </div>
